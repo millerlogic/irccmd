1225d4c41cdf8327e72fb7b1cf52e2f171200b07
Anders Bergh <anders1@gmail.com>
2014-05-04 05:08:33 +0200

    Add premake4 script.

---
+++ premake4.lua
@@ -0,0 +1,33 @@
+solution "irccmd"
+	configurations { "Debug", "Release" }
+
+	project "irccmd"
+		kind "ConsoleApp"
+		language "C"
+		files { "**.h", "**.c" }
+
+		-- Find the proper Lua pkg-config in this order.
+		local lua_names = { "lua5.1", "luajit", "lua" }
+
+		local lua_name
+		for _, name in ipairs(lua_names) do
+			if os.execute("pkg-config "..name) == 0 then
+				lua_name = name
+				break
+			end
+		end
+
+		if lua_name then
+			buildoptions { "`pkg-config "..lua_name.." --cflags`" }
+			linkoptions { "`pkg-config "..lua_name.." --libs`" }
+		else
+			-- no pkg-config info installed, just try Lua
+			links { "lua" }
+		end
+
+		configuration "Debug"
+			defines { "_DEBUG" }
+			flags { "Symbols" }
+
+		configuration "Release"
+			flags { "Optimize" }
